73ee12c3450ae868c6c4178769b078a325ae238e
Francisco Anzueto <francisco.anzueto@xoom.com>
2016-09-26 12:26:25 -0600

    Create and display a custom view

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,18 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Francisco Anzueto on 9/9/16.
+//  Copyright © 2016 Francisco Anzueto. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+    // MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+}
