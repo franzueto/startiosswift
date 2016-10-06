9b1671aa939f6a249fdf28ce74cb9281373f6e09
Francisco Anzueto <francisco.anzueto@xoom.com>
2016-10-06 16:14:52 -0600

    Add and Configure MealTableViewCell

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Francisco Anzueto on 10/6/16.
+//  Copyright © 2016 Francisco Anzueto. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
