c1202e75dc1c4063fac4aa5bffc49e50fa1adaf8
Francisco Anzueto <francisco.anzueto@xoom.com>
2016-10-07 15:32:15 -0600

    Show meal info on Meal Screen when clicking an item on the list

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -31,6 +31,13 @@ class MealViewController: UIViewController, UITextFieldDelegate,
         super.viewDidLoad()
         nameTextField.delegate = self
         
+        if let meal = meal {
+            navigationItem.title = meal.name
+            nameTextField.text   = meal.name
+            photoImageView.image = meal.photo
+            ratingControl.rating = meal.rating
+        }
+        
         // Enable the Save button only if the text field has a valid Meal name.
         checkValidMealName()
         
