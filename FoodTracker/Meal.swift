9d2e7452bf064a780fa282e0e9a57815254f4289
Francisco Anzueto <francisco.anzueto@xoom.com>
2016-10-10 11:03:25 -0600

    Implement NSObject and NSCoding protocol in Meal class to be able to save it in local storage

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -8,12 +8,23 @@
 
 import UIKit
 
-class Meal {
+class Meal: NSObject, NSCoding {
+    
+    // MARK: Types
+    struct PropertyKey {
+        static let nameKey = "name"
+        static let photoKey = "photo"
+        static let ratingKey = "rating"
+    }
     
     // MARK: Properties
     var name: String
     var rating: Int
     var photo: UIImage?
+    
+    // MARK: Archiving Paths
+    static let DocumentsDirectory = NSFileManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.URLByAppendingPathComponent("meals")
     
     // MARK: Initialization
     init?(name: String, rating: Int, photo: UIImage?) {
@@ -21,8 +32,30 @@ class Meal {
         self.rating = rating
         self.photo = photo
         
+        super.init()
+        
+        // Initialization should fail if there is no name or if the rating is negative.
         if name.isEmpty || rating < 0 {
             return nil
         }
+    }
+    
+    // MARK: NSCoding
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(name, forKey: PropertyKey.nameKey)
+        aCoder.encodeObject(photo, forKey: PropertyKey.photoKey)
+        aCoder.encodeInteger(rating, forKey: PropertyKey.ratingKey)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        let name = aDecoder.decodeObjectForKey(PropertyKey.nameKey) as! String
+        
+        // Because photo is an optional property of Meal, use conditional cast.
+        let photo = aDecoder.decodeObjectForKey(PropertyKey.photoKey) as? UIImage
+        
+        let rating = aDecoder.decodeIntegerForKey(PropertyKey.ratingKey)
+        
+        // Must call designated initializer.
+        self.init(name: name, rating: rating, photo: photo)
     }
 }
